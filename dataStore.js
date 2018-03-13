9f6de3991598032d5708f5b6f80661247c5a836a
Tarik Mey <tarik-mey@gmx.de>
2018-03-13 07:23:27 +0100

    Added the unmodified files

---
+++ dataStore.js
@@ -0,0 +1,118 @@
+const questions = [
+    // Question 1
+    {
+        question: "Which Volcano killed the most people?",
+        answers: [
+            "Mount Tambora, Indonesia, 1815", //>> yes killed 71,000+ 
+            "Mount Vesuvius, Italy, 79",
+            "Mount Pelée, Martinique, 1902",
+            "Mount Unzen, Japan, 1792"
+        ]
+    },
+    // Question 2
+    {
+        question: "Which material gives Arequipa (Peru) the name 'White city'?",
+        answers: [
+            "sillar", //right
+            "salt",
+            "chalk",
+            "sandstone"
+        ]
+    },
+    // Question 3
+    {
+        question: "What is a dormant volcano?",
+        answers: [
+            "A quiet one that might erupt again", // correct
+            "A cold volcano",
+            "A volcano recently erupted",
+            "A dead volcano"
+        ]
+    },
+    // Question 4
+    {
+        question: "Which volcano has the largest lava lake on earth?",
+        answers: [
+            "Mount Nyiragongo, DR Congo", // correct
+            "Galeras, Colombia",
+            "Mount Merapi, Indonesia",
+            "Mount Rainier, USA"
+        ]
+    },
+    //Question 5
+    {
+        question: "Which is the largest volcano in Europe?",
+        answers: [
+            "Mount Etna, Italy", // correct
+            "Pico Alto, Portugal",
+            "Beerenberg, Norway",
+            "Mounts of Cantal, France"
+        ]
+    },
+    //Question 6
+    {
+        question: "Which is the highest active volcano in Eurasia?",
+        answers: [
+            "Mount Klyuchevskaya Sopka, Russia", // correct
+            "Krakatoa, Indonesia",
+            "Mount Fuji, Japan",
+            "Mount Etna, Italy"
+        ]
+    },
+    //Question 7
+    {
+        question: "Which volcanic material lets beaches appear black?",
+        answers:[
+            "basalt", //correct
+            "sillar",
+            "bauxite",
+            "rhyolite"
+        ]
+    }, 
+    //Question 8
+    {
+        question: "Which is NOT one of four ways to get killed by a volcano?",
+        answers: [
+            "Death by drowning in lava", //correct
+            "Death by carbon dioxide", 
+            "Death by mudflows",
+            "Death by pyroclastic flows",
+        ]
+    }, 
+    //Question 9
+    {
+        question: "How fast can pyroclastic flows travel?",
+        answers: [
+            "450mph or 724km/h", //correct
+            "200mph or 322km/h",
+            "687mph or 1106km/h",
+            "to fast to measure"
+        ]
+    },
+    //Question 10
+    {
+        question: "What are lahars?",
+        answers: [
+            "volcanic mudflows", //correct
+            "volcanic eruptions",
+            "volcanic explosions",
+            "volcanic rocks"
+        ]
+    }
+];
+
+
+
+const answers = [
+    "Mount Tambora, Indonesia, 1815",
+    "sillar",
+    "A quiet one that might erupt again",
+    "Mount Nyiragongo, DR Congo",
+    "Mount Etna, Italy",
+    "Mount Klyuchevskaya Sopka, Russia",
+    "basalt",
+    "Death by drowning in lava",
+    "450mph or 724km/h",
+    "volcanic mudflows",
+];
+
